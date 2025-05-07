a05c2e5384a02f35196fdeb137866ba5a1674de4
julian7890 <64969120+julian7890@users.noreply.github.com>
2025-05-07 19:01:31 -0400

    Update schema.tsx

--- drizzle/schema.tsx
+++ drizzle/schema.tsx
@@ -9,7 +9,6 @@ import {
   time,
   timestamp,
   boolean,
-  json,
 } from "drizzle-orm/pg-core";
 
 export const PlayerTable = pgTable(
@@ -65,7 +64,6 @@ export const GameTable = pgTable("game", {
   selfResult: varchar("selfResult", { length: 255 }),
   opponentResult: varchar("opponentResult", { length: 255 }),
   homeTeam: boolean("homeTeam"),
-  order: json("order"),
 });
 
 export const GameStatTable = pgTable(
